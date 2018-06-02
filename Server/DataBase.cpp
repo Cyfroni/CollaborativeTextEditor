8ff2422b3ae848a63d41f312ea86e2aceea65cb6
Cyfroni <karolplg@gmail.com>
2018-06-02 23:56:46 +0200

    code refactor, splitted code to folders

---
+++ Server/DataBase.cpp
@@ -0,0 +1,202 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <regex>
+
+using namespace std;
+
+#define DIR_PATH "./Files/"
+
+typedef vector<char> LINE;
+typedef unordered_set<int> LISTENERS;
+typedef vector<LINE> SHEET;
+typedef pair<SHEET, LISTENERS> DOCK;
+typedef unordered_map<string, DOCK> DATABASE;
+
+void add(DATABASE &, string, string);
+void printSheet(SHEET);
+SHEET bufferFile(string);
+vector<string> split(const string&, const string&);
+
+void test()
+{
+	string file = "kohan.txt";
+	LINE line1{ 'a' }, line2{ '2','q' };
+	SHEET sheet{LINE{}};
+	LISTENERS lis({ 1,2 });
+
+	DOCK dock = make_pair(sheet, lis);
+
+	DATABASE dataBase;
+	dataBase.emplace(file, dock);
+	//przychodzi info od klienta
+	//string info = "1.0.1.1:t";
+	string info1 = "1.0.1.1:t";
+	// ab4
+	// 2q
+
+	// abtg
+	// w4
+	// 2q
+
+	printSheet(dataBase[file].first);
+	add(dataBase, file, info1);
+	printSheet(dataBase[file].first);
+}
+
+void updateFile(string fileName, SHEET sheet)
+{
+	fstream fileStream;
+	fileStream.open(DIR_PATH + fileName, ios::out | ios::trunc);
+	if (fileStream.good())
+	{
+		printf("Opening for update went OK \n");
+		for (auto i = sheet.begin(); i != sheet.end(); i++)
+		{
+			for (auto j = (*i).begin(); j != (*i).end(); j++)
+			{
+				fileStream << *j;
+			}
+			fileStream << endl;
+		}
+		fileStream.flush();			/*Forcing buffer to go to harddisc's emory*/
+		fileStream.close(); 			/*Close the file when finished*/
+		printf("Everything is OK \n");
+	}
+	else
+	{
+		printf("SOMETHING IS WRONG\n");
+	}
+}
+
+void add(DATABASE &dataBase, string file, string info)
+{
+	regex re1(":"), re2("[0-9]+"), re3("[ -~]*"), re4("\n");
+	int startIndex[2], endIndex[2];
+	string data, change;
+	smatch tmp;
+
+	regex_search(info, tmp, re1);
+	data = tmp.prefix().str();
+	change = tmp.suffix().str();
+
+	sregex_iterator next(data.begin(), data.end(), re2);
+	startIndex[0] = atoi((*next).str().c_str());
+	startIndex[1] = atoi((*(++next)).str().c_str());
+	endIndex[0] = atoi((*(++next)).str().c_str());
+	endIndex[1] = atoi((*(++next)).str().c_str());
+
+	cout << "#" << change << "#\n";
+	int linesAmount = endIndex[0] - startIndex[0];
+
+
+	DOCK &dock = dataBase[file];
+	SHEET &sheet = dock.first;
+
+	if (change == "")
+	{
+		if (linesAmount == 0)
+		{
+			LINE &line = sheet[startIndex[0] - 1];
+			line.erase(line.begin() + startIndex[1], line.begin() + endIndex[1]);
+		}
+		else
+		{
+			LINE &line = sheet[startIndex[0] - 1];
+			LINE &lineEnd = sheet[endIndex[0] - 1];
+			line.erase(line.begin() + startIndex[1], line.end());
+			line.insert(line.end(), lineEnd.begin() + endIndex[1], lineEnd.end());
+			sheet.erase(sheet.begin() + startIndex[0], sheet.begin() + endIndex[0]);
+		}
+	}
+	else
+	{
+		if(sheet.size()==0 && linesAmount==0)
+			linesAmount=1;
+		vector<LINE> newLines(linesAmount, LINE{});
+		sheet.insert(sheet.begin() + startIndex[0], newLines.begin(), newLines.end());
+
+		auto vecLines = split(change, "\n");
+		int q = 0;
+		int index = startIndex[1];
+		for (auto str : vecLines)
+		{
+			printSheet(sheet);
+			if (q == 0 && linesAmount > 0) {
+				LINE &line = sheet[startIndex[0] - 1];
+				LINE &lineEnd = sheet[endIndex[0] - 1];
+				vector<char> vec(str.begin(), str.end());
+				lineEnd.insert(lineEnd.begin(), line.begin() + startIndex[1], line.end());
+				line.erase(line.begin() + startIndex[1], line.end());
+				line.insert(line.begin() + index, vec.begin(), vec.end());
+			}
+			else {
+				cout<<"1"<<endl;
+				LINE &line = sheet[startIndex[0] - 1 + q];
+				cout<<"2"<<endl;
+				vector<char> vec(str.begin(), str.end());
+				cout<<"3"<<endl;
+				line.insert(line.begin() + index, vec.begin(), vec.end());
+				cout<<"4"<<endl;
+			}
+			index = 0;
+			q++;
+		}
+	}
+}
+
+void printSheet(SHEET sheet)
+{
+	cout << "POCZATEK PLIKU" << endl;
+	for (auto i = sheet.begin(); i != sheet.end(); i++)
+	{
+		for (auto j = (*i).begin(); j != (*i).end(); j++)
+		{
+			cout << *j;
+		}
+		cout << endl;
+	}
+	cout << "KONIEC PLIKU" << endl;
+
+}
+
+SHEET bufferFile(string fileName)
+{
+	SHEET sheet;
+	ifstream file;
+	string line;
+	file.open((string)DIR_PATH + fileName);
+	if (file.is_open())
+	{
+		printf("bufferFile\n");
+		while (getline(file, line))
+		{
+			vector<char> vec(line.begin(), line.end());
+			sheet.emplace_back(vec);
+		}
+		file.close();
+	}
+	cout<<"soz"<<sheet.size()<<endl;
+	if(sheet.size()==0)
+		sheet.push_back(LINE{});
+	printSheet(sheet);
+	return sheet;
+}
+
+vector<string> split(const string& str, const string& delim)
+{
+	vector<string> tokens;
+	size_t prev = 0, pos = 0;
+	do
+	{
+		pos = str.find(delim, prev);
+		if (pos == string::npos) pos = str.length();
+		string token = str.substr(prev, pos - prev);
+		tokens.push_back(token);
+		prev = pos + delim.length();
+	} while (pos < str.length() && prev < str.length());
+	return tokens;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,312 @@
+//g++ -std=c++11 Server.cpp -lpthread ; echo 1 ; ./a.out
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <deque>
+#include <dirent.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include "test.cpp"
+#include <netdb.h>
+
+#define MYPORT 8080    // port, z którym będą się łączyli użytkownicy
+#define MYPORT1 8090
+#define BACKLOG 10     // jak dużo możę być oczekujących połączeń w kolejce
+#define MAX_LENGTH 20
+
+struct stat info;
+deque<string> FileList;
+unordered_map<int, int> ChildrenSockets;
+pthread_t thread_id;
+DATABASE dataBase;
+deque<pair<string, string> > messageQueue;
+
+struct arg_struct {
+	int sock;
+	int PORT_num;
+};
+
+void *connection_handler(void *);
+void readDocumentNames(const string&, deque<string>&);
+void sigchld_handler(int);
+void *listening(void*)
+
+int main(int c, char** v)
+{
+	if (c > 1)
+	{
+		test();
+		return 0;
+	}
+	int root_socket; // nasłuchuj na root_socket, nowe połaczenia na sock
+	struct sockaddr_in my_addr; // informacja o moim adresie
+	struct sockaddr_in their_addr; // informacja o adresie osoby łączącej się
+	unsigned int sin_size;
+	struct sigaction sa;
+	int yes = 1;
+
+	if ((root_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("socket");
+		exit(1);
+	}
+
+	if (setsockopt(root_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+	{
+		perror("setsockopt");
+		exit(1);
+	}
+
+	my_addr.sin_family = AF_INET; // host byte order
+	my_addr.sin_port = htons(MYPORT); // short, network byte order
+	my_addr.sin_addr.s_addr = INADDR_ANY; // uzupełnij moim adresem IP
+	memset(&(my_addr.sin_zero), '\0', 8); // wyzeruj resztę struktury
+
+	if (bind(root_socket, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("bind");
+		exit(1);
+	}
+
+	if (listen(root_socket, BACKLOG) == -1)
+	{
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // zbierz martwe procesy
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(1);
+	}
+
+	pthread_create(&thread_id, NULL, listening, 0);
+	readDocumentNames(DIR_PATH, FileList);
+	int port_generator = 9000;
+	while (1)
+	{ // głowna pętla accept()
+		sin_size = sizeof(struct sockaddr_in);
+		struct arg_struct args;
+		if ((args.sock = accept(root_socket, (struct sockaddr *)&their_addr, &sin_size)) == -1)
+		{
+			perror("accept");
+			continue;
+		}
+		args.PORT_num = port_generator++;
+		printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
+		pthread_create(&thread_id, NULL, connection_handler, &args);
+		pthread_detach(thread_id);
+
+		int mother_socket; // nasłuchuj na sock_fd, nowe połaczenia na sock
+
+		int yes = 1;
+
+		if ((mother_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		{
+			perror("socket");
+			continue;
+		}
+
+		if (setsockopt(mother_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+		{
+			perror("setsockopt");
+			continue;
+		}
+
+		struct sockaddr_in addrRemote; // informacja o adresie osoby łączącej się
+		memset(&addrRemote, 0, sizeof(struct sockaddr_in));
+		addrRemote.sin_port = htons((port_generator - 1));
+		addrRemote.sin_family = AF_INET;
+		addrRemote.sin_addr.s_addr = htonl(INADDR_ANY);
+
+		while (connect(mother_socket, (sockaddr*)&addrRemote, sizeof(addrRemote)) == -1)
+		{
+			sleep(1);
+			cout << "RIP" << endl;
+		}
+		ChildrenSockets.emplace(args.sock, mother_socket);
+	}
+	return 0;
+}
+
+void readDocumentNames(const string& dirName, deque<string>& list)
+{
+	DIR* dirp = opendir(dirName.c_str());
+	struct dirent *dp;
+	while ((dp = readdir(dirp)) != NULL)
+	{
+		if (*(dp->d_name) != '.' && *(dp->d_name) != '.' + '.')
+			list.push_back(dp->d_name);
+	}
+	closedir(dirp);
+}
+
+void *listening(void*)
+{
+	pair<string, string> message;
+
+	while (1)
+	{
+		if (!messageQueue.empty())
+		{
+			message = messageQueue.front();
+			messageQueue.pop_front();
+			auto &listeners = dataBase[message.second].second;
+			string info = message.first;
+			cout << "Matula:  " << info << "###";
+			for (auto x : listeners)
+			{
+				cout << x << " ";
+				send(ChildrenSockets[x], info.c_str(), strlen(info.c_str()), 0);
+				add(dataBase, message.second, info);
+			}
+			cout << endl;
+		}
+
+	}
+
+}
+
+void sigchld_handler(int s)
+{
+	while (wait(NULL) > 0);
+}
+
+void *connection_handler(void* socket_desc)
+{
+	// to jest proces-dziecko
+	struct arg_struct arg = *(struct arg_struct*)socket_desc;
+	int sock = arg.sock;
+	string fileOpen = "";
+	sleep(1);
+	if (send(sock, &arg.PORT_num, sizeof(int), 0) == -1)
+		perror("send");
+	perror("wyslane\n");
+	int amount;
+	char instr[MAX_LENGTH];
+
+	if (stat(DIR_PATH, &info) != 0)
+		system((string("mkdir -p ") + DIR_PATH).c_str());
+	else if (info.st_mode & S_IFDIR) {}
+	else
+		perror((string("") + DIR_PATH + " is no directory\n").c_str());
+
+	while (1)
+	{
+		perror("czekam\n");
+		if ((amount = recv(sock, instr, sizeof(instr) - 1, 0)) == -1)
+			perror("recv");
+
+		if (amount == 0)
+			break;
+
+		instr[amount] = '\0';
+
+		printf("%d: %s  \n", amount, instr);
+		if (strlen(instr) < 2)
+		{
+			cout << "zbyt krotka wiadomosc" << endl;
+			continue;
+		}
+
+		if (instr[0] == 'N')
+		{
+			int fWrongName = 0;
+			for (int i = 0; i < MAX_LENGTH - 1; i++)
+				instr[i] = instr[i + 1];
+			for (int j = 0; j < FileList.size(); j++)
+			{
+				if (FileList[j] == ((string)instr).append(".txt"))
+				{
+					fWrongName = 1;
+				}
+
+			}
+			if (!fWrongName)
+			{
+				FileList.push_back(((string)instr).append(".txt"));
+				fstream fileStream.open(DIR_PATH + FileList[FileList.size() - 1], ios::out);
+				if (fileStream.good())
+				{
+					perror("Creation went OK \n");
+					fileStream << "\0";
+					fileStream.flush();			/*Forcing buffer to go to harddisc's memory*/
+					fileStream.close(); 			/*Close the file when finished*/
+				}
+				else
+					perror("Error in file creation\n");
+			}
+			else
+			{
+				perror("File with such a name already exists\n");
+				fWrongName = 0;
+			}
+
+		}
+		if (!strcmp(instr, "UP"))
+		{
+			if (FileList.size() == 0)
+				send(sock, "\n", strlen("\n"), 0);
+			for (int j = 0; j < FileList.size(); j++)
+			{
+				send(sock, (FileList[j] + "\n").c_str(), strlen((FileList[j] + "\n").c_str()), 0);
+				cout << FileList[j] + "\n";
+			}
+		}
+		if (instr[0] == 'G')
+		{
+			string info(instr + 1, strlen(instr) - 1);
+			ifstream file;
+			string line;
+			file.open((string)DIR_PATH + info);
+			if (file.is_open())
+			{
+				printf("plik otwarty\n");
+				while (getline(file, line))
+				{
+					send(sock, (line + "\n").c_str(), strlen((line + "\n").c_str()), 0); //TODO: this should send data from dataBase, not file
+				}
+				send(sock, "\0", sizeof(char), 0);
+				file.close();
+				fileOpen = info;
+				if (dataBase.count(fileOpen) > 0)
+					dataBase[fileOpen].second.insert(sock);
+				else
+				{
+					LISTENERS lis({ sock });
+					SHEET sheet = bufferFile(fileOpen);
+					DOCK dock = make_pair(sheet, lis);
+					dataBase.emplace(fileOpen, dock);
+				}
+			}
+		}
+		if (!strcmp(instr, "UG")) //close file
+		{
+			dataBase[fileOpen].second.erase(sock);
+			cout << dataBase[fileOpen].second.size() << endl;
+			if (dataBase[fileOpen].second.size() == 0)
+			{
+				DOCK &dock = dataBase[fileOpen];
+				SHEET &sheet = dock.first;
+				updateFile(fileOpen, sheet);
+				//TODO: remove dock from database - need to clear data
+			}
+			fileOpen = "";
+		}
+		if (instr[0] == 'Z')
+		{
+			string info(instr + 1, strlen(instr) - 1);
+			messageQueue.emplace_back(info, fileOpen);
+
+		}
+
+	}
+	//TODO: potrzebna obsługa jak ktos sie odlączy
+	close(sock);
+	cout << "hahahah" << endl;
+}
