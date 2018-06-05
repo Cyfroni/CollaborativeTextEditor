a7ff5c8211d6ae9459dbab7ec987650eb06191a1
Frustrado <maksymiliankrysa@gmail.com>
2018-06-05 17:21:45 +0200

    watki i odbior wiadomosci od serwera - nie wiadomo czy dziala

--- WebClient/src/main/java/collaborativetexteditor/webclient/controller/AppController.java
+++ WebClient/src/main/java/collaborativetexteditor/webclient/controller/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteOrder;
 import java.nio.CharBuffer;
@@ -19,11 +20,13 @@ public class AppController {
 
     Socket sock;
     PrintWriter out;
-    //BufferedReader in;
     DataInputStream in;
-    char c;
     byte[] buffer=new byte[1024];
-    int x;
+    int port;
+    String mess;
+    ServerSocket serverSocket;
+    Socket clientSocket;
+    StringBuffer sb = new StringBuffer();
     AppController(){
         super();
         create();
@@ -34,17 +37,36 @@ public class AppController {
         try {
             sock = new Socket("127.0.0.1", 8080);
             out = new PrintWriter(sock.getOutputStream(), true);
-            //in = sock.getInputStream();// new BufferedReader(new InputStreamReader(sock.getInputStream()),4);
             DataInputStream in = new DataInputStream(sock.getInputStream());
-            System.out.println(in.available());
             System.out.println(in.read(buffer));
-            System.out.println(in.available());
-            System.out.println(buffer[0]);
-            System.out.println(buffer[1]);
-            System.out.println((buffer[0]+buffer[1]*pow(2,8)));
+            port= (int)(buffer[0] + (buffer[1] * pow(2, 8)));
+            System.out.println(port);
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            DataInputStream infrom = new DataInputStream(clientSocket.getInputStream());
+            Thread thread = new Thread("clientSocket"){
+                public void run(){
+                    while(true) {
+                        System.out.println("czeka");
+                        try {
+                            char c;
+                            while(true) {
+                                c=infrom.readChar();
+                                sb.append(c);
+                            }
 
-
-        } catch (IOException e1) {
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (mess.equals(""))
+                            break;
+                        System.out.println("#"+ mess);
+                        //queue.append(info)
+                    }
+                }
+            };
+            thread.start();
+            } catch (IOException e1) {
             e1.printStackTrace();
         }
     }
